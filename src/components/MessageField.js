9fefeb9584e9066ecc0dac847e59fa594dfd07f0
takosan8 <yukiwebcreate@gmail.com>
2022-02-17 23:52:26 +0900

    adjust focusing

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { TextField } from "@material-ui/core";
 
 import {pushMessage } from "../firebase";
 
-const MessageField = ({name, setText, text}) => {
+const MessageField = ({inputEl, name, setText, text}) => {
+  
     const [isComposed, setIsComposed] = useState(false);
     return(
 
      <TextField
+        autoFocus
         fullWidth={true}
+        inputRef={inputEl}
         onChange={(e)=>{
             setText(e.target.value);
         }}  
